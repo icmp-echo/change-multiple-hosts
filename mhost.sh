657ace404c1f26cabf1de0498a3397d496812a7d
icmp-echo <anktvora@yahoo.com>
2019-01-03 16:14:07 -0500

    Create mhost.sh

---
+++ mhost.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+hosts="test-cisco test-brocade"           #could be any number of host one space between each host in quotes 
+for hosts in $hosts; do
+    expect /home/avora/change.exp $hosts  #path to your change.exp file
+done
+wait
